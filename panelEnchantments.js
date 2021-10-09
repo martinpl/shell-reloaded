4c6ce4b794963425d4a105e8c9874917ed0e37e1
Martin <martinplr@gmail.com>
2021-10-09 20:12:05 +0200

    feat: full leftBox width

--- panelEnchantments.js
+++ panelEnchantments.js
@@ -1,7 +1,79 @@
-const { Gio, St } = imports.gi;
+const { Clutter, Gio, St } = imports.gi;
+const Gi = imports._gi;
 const Me = imports.misc.extensionUtils.getCurrentExtension();
 const { WorkspacesBar } = Me.imports.taskbar;
 const Main = imports.ui.main;
+
+function vfunc_allocate(box, flags) {
+	this.set_allocation(box, flags);
+
+	let allocWidth = box.x2 - box.x1;
+	let allocHeight = box.y2 - box.y1;
+
+	let [leftMinWidth, leftNaturalWidth] = this._leftBox.get_preferred_width(-1);
+	let [centerMinWidth, centerNaturalWidth] =
+		this._centerBox.get_preferred_width(-1);
+	let [rightMinWidth, rightNaturalWidth] =
+		this._rightBox.get_preferred_width(-1);
+
+	let sideWidth = allocWidth - rightNaturalWidth - centerNaturalWidth;
+
+	let childBox = new Clutter.ActorBox();
+
+	childBox.y1 = 0;
+	childBox.y2 = allocHeight;
+	if (this.actor.get_text_direction() == Clutter.TextDirection.RTL) {
+		childBox.x1 =
+			allocWidth - Math.min(Math.floor(sideWidth), leftNaturalWidth);
+		childBox.x2 = allocWidth;
+	} else {
+		childBox.x1 = 0;
+		childBox.x2 = Math.min(Math.floor(sideWidth), leftNaturalWidth);
+	}
+	this._leftBox.allocate(childBox, flags);
+
+	childBox.y1 = 0;
+	childBox.y2 = allocHeight;
+	if (this.actor.get_text_direction() == Clutter.TextDirection.RTL) {
+		childBox.x1 = rightNaturalWidth;
+		childBox.x2 = childBox.x1 + centerNaturalWidth;
+	} else {
+		childBox.x1 = allocWidth - centerNaturalWidth - rightNaturalWidth;
+		childBox.x2 = childBox.x1 + centerNaturalWidth;
+	}
+	this._centerBox.allocate(childBox, flags);
+
+	childBox.y1 = 0;
+	childBox.y2 = allocHeight;
+	if (this.actor.get_text_direction() == Clutter.TextDirection.RTL) {
+		childBox.x1 = 0;
+		childBox.x2 = rightNaturalWidth;
+	} else {
+		childBox.x1 = allocWidth - rightNaturalWidth;
+		childBox.x2 = allocWidth;
+	}
+	this._rightBox.allocate(childBox, flags);
+
+	let [leftCornerMinWidth, leftCornerWidth] =
+		this._leftCorner.actor.get_preferred_width(-1);
+	let [leftcornerMinHeight, leftCornerHeight] =
+		this._leftCorner.actor.get_preferred_width(-1);
+	childBox.x1 = 0;
+	childBox.x2 = leftCornerWidth;
+	childBox.y1 = allocHeight;
+	childBox.y2 = allocHeight + leftCornerHeight;
+	this._leftCorner.actor.allocate(childBox, flags);
+
+	let [rightCornerMinWidth, rightCornerWidth] =
+		this._rightCorner.actor.get_preferred_width(-1);
+	let [rightCornerMinHeight, rightCornerHeight] =
+		this._rightCorner.actor.get_preferred_width(-1);
+	childBox.x1 = allocWidth - rightCornerWidth;
+	childBox.x2 = allocWidth;
+	childBox.y1 = allocHeight;
+	childBox.y2 = allocHeight + rightCornerHeight;
+	this._rightCorner.actor.allocate(childBox, flags);
+}
 
 function panelEnchantments(panel, monitor) {
 	if (panel.statusArea["activities"]) {
@@ -50,6 +122,8 @@ function panelEnchantments(panel, monitor) {
 			rightBox.insert_child_at_index(dateMenu.container, children.length - 1);
 		}
 	}
+
+	Main.panel.__proto__[Gi.hook_up_vfunc_symbol]("allocate", vfunc_allocate);
 }
 
 function disablePanelEnchantments(panel) {
@@ -83,4 +157,9 @@ function disablePanelEnchantments(panel) {
 			centerBox.add_actor(dateMenu.container);
 		}
 	}
+
+	Main.panel.__proto__[Gi.hook_up_vfunc_symbol](
+		"allocate",
+		Main.panel.__proto__.vfunc_allocate
+	);
 }
