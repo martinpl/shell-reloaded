50c85f848a5a523a09a10f0c5a3854a3364d804b
Martin <martinplr@gmail.com>
2023-10-31 20:32:46 +0100

    fix: taskbar being on wrong side of active button after hibernation

--- panel/taskBar.js
+++ panel/taskBar.js
@@ -3,7 +3,7 @@ import { taskbar } from "./taskbar/taskbar.js"
 export const taskBar = {
     enable(panel) {
         panel._taskbar = new taskbar(panel._monitor?.index)
-        panel.addToStatusArea("taskbar", panel._taskbar, 9, "left")
+        panel.addToStatusArea("taskbar", panel._taskbar, -1, "left")
     },
     disable(panel) {
         panel._taskbar.destroy()
